d0e72bf016848164a0357fcd12dbe44c9ab3e196
beszpremidori <notpublic@n.com>
2019-10-13 11:32:30 +0200

    create makefile and separated main.cpp

--- terminal/main.cpp
+++ terminal/main.cpp
@@ -1,49 +1,8 @@
 #include <iostream>
 #include<string>
 #include <cstdlib>
-
+#include "Tree.h"
 using namespace std;
-
-class Node
-{
-public:
-	Node* parent;
-	Node* child[256];
-	int childcount = 0;
-	string name;
-
-	Node()
-	{
-		parent = NULL;
-		for (int i = 0; i < 256; i++) 
-		{
-			child[i] = NULL;
-		}
-		name = "root";
-		
-	}
-
-};
-
-class Tree
-{
-public: 
-	Node* head;
-	Node* root;
-	string path = "root>";
-	Tree()
-	{
-		head = new Node();
-		root = head;
-		head->name = "root";
-	}
-
-	void add(string name);
-	void ls();
-	void list(Node* node);
-	void cd(Node* node, string name);
-	void up(Node* node);
-};
 
 int main(void)
 {
@@ -86,65 +45,4 @@ int main(void)
 	} while (!exit);
 }
 
-void Tree::add(string name)
-{
-	Node* n = new Node();
-	head->child[head->childcount] = n;
-	head->childcount++;
-	n->parent = head;
-	n->name = name;
-	//system("cls");
-}
 
-void Tree::ls()
-{
-	Node* n = root;
-	for (int i = 0; n->child[i] != NULL; i++)
-	{
-		cout << n->child[i]->name << endl;
-	}
-}
-
-void Tree::list(Node* node)
-{
-	Node* n = node;
-	
-	for (int i = 0; n->child[i] != NULL; i++)
-	{
-		cout << n->child[i]->name << endl;
-	}
-}
-void Tree::cd(Node* node ,string name)
-{
-	Node* n = node;
-	bool success = false;
-	for (int i = 0; n->child[i] != NULL; i++)
-	{
-		if (n->child[i]->name == name)
-		{
-			head = n->child[i];
-			path += name + ">";
-			success = true;
-		}
-
-	}
-	if (success == false)
-	{
-		cout << name << " DIR not exist!" << endl;
-	}
-	
-}
-
-void Tree::up(Node* node)
-{
-	if (root != head)
-	{
-		head = node->parent;
-		path = path.substr(0, path.find_last_of('>'));
-		path = path.substr(0, path.find_last_of('>') + 1);
-	}
-	else
-	{
-		cout << "You are in the root directory" << endl;
-	}
-}
